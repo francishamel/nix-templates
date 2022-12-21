2ebd72a427deb498daa8e5367ee06d99b790f9a5
Francis Hamel <francishamel96@gmail.com>
2022-12-21 12:09:02 -0500

    Setup dev shell for templates (#2)

--- flake.nix
+++ flake.nix
@@ -1,13 +1,42 @@
-
 {
   description = "francishamel's flake templates";
 
-  outputs = { self }: {
-    templates = {
-      elixir = {
-        path = ./templates/elixir;
-        description = "A flake for an elixir environment.";
-      };
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { flake-utils, nixpkgs, self }:
+    let
+      inherit (flake-utils.lib) defaultSystems eachSystemMap;
+    in
+    {
+      legacyPackages = eachSystemMap defaultSystems (system:
+        import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        }
+      );
+
+      templates = {
+        elixir = {
+          path = ./templates/elixir;
+          description = "A flake for an elixir environment.";
+        };
+      };
+
+      devShells = eachSystemMap defaultSystems (system:
+        let
+          pkgs = self.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nil
+              nixpkgs-fmt
+            ];
+          };
+        }
+      );
+    };
 }
